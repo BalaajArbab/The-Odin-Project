6fbe7bdc1778a5b2dbab86c9d093e41f6ef38257
BalaajArbab <Balaaj.Arbab@gmail.com>
2022-08-21 10:06:19 -0400

    Initial Calculator layout

--- Foundations/Project 5: Calculator/Program.js
+++ Foundations/Project 5: Calculator/Program.js
@@ -1,1 +1,26 @@
 "use strict"
+
+const Add = (a, b) => a + b;
+const Subtract = (a, b) => a - b;
+const Multiply = (a, b) => a * b;
+const Divide = (a, b) => b !== 0 ? a / b : "ZeroDivide";
+
+function Operate(operator, a, b)
+{
+    switch(operator)
+    {
+        case "+":
+            Add(a, b);
+            break;
+        case "-":
+            Subtract(a, b);
+            break;
+        case "*":
+            Multiply(a, b);
+            break;
+        case "/":
+            Divide(a, b);
+        default:
+            console.error("No operator.");
+    }
+}
