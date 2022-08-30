1d0d461085ff98148e8f647a48f9a1487aa796a2
BalaajArbab <Balaaj.Arbab@gmail.com>
2022-08-29 21:45:13 -0400

    Design Implemented.

--- FullstackJavaScriptPath/IntermediateHTMLAndCSS/Project 6: Sign-Up Form/Program.js
+++ FullstackJavaScriptPath/IntermediateHTMLAndCSS/Project 6: Sign-Up Form/Program.js
@@ -1,2 +1,29 @@
 "use strict";
 
+const passwordInput = document.querySelector("#Password");
+const confirmPasswordInput = document.querySelector("#Confirm");
+const invalidPWText = document.querySelector("#InvalidPW");
+
+confirmPasswordInput.addEventListener("input", ComparePWs);
+
+function ComparePWs(e)
+{
+    const pwString = passwordInput.value;
+    const confirmString = confirmPasswordInput.value;
+
+    if (pwString !== confirmString) 
+    {
+        passwordInput.classList.add("Error");
+        confirmPasswordInput.classList.add("Error");
+        invalidPWText.classList.remove("Hide");
+    }
+    else
+    {
+        passwordInput.classList.remove("Error");
+        confirmPasswordInput.classList.remove("Error");
+        invalidPWText.classList.add("Hide");
+    }
+
+    console.log(pwString, confirmString);
+    console.dir(passwordInput);
+}
